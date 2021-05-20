594eb9b6c08c7482f7732d1cbb4a1aa77db653b6
sunhpark42 <sunhpark42@gmail.com>
2021-05-20 18:00:55 +0900

    feat: Box 컴포넌트 구현

---
+++ src/components/shared/Box/Box.stories.tsx
@@ -0,0 +1,21 @@
+import Box, { BoxProps } from './Box';
+import { Story } from "@storybook/react";
+
+export default {
+  title: 'components/shared/Box',
+  component: Box,
+};
+
+const Template: Story<BoxProps> = (args) => <Box {...args} />;
+
+export const Default = Template.bind({});
+export const WithHat = Template.bind({});
+
+Default.args = {
+  children: <div style={{ height: "300px" }}></div>,
+};
+
+WithHat.args = {
+  hatColor: '#0dd273',
+  children: <div style={{ height: "300px" }}></div>,
+}
---
+++ src/components/shared/Box/Box.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+import { Container, ContainerProps } from './Box.style';
+
+interface BoxProps extends ContainerProps {
+  children: ReactNode;
+}
+
+const Box = ({ hatColor, children }: BoxProps) => (
+  <Container hatColor={hatColor}>
+    {children}
+  </Container>
+);
+
+export default Box;
+export type { BoxProps };
