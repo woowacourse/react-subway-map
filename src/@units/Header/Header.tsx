df461307136b0453ac63bdfeea44b9dc37515350
yujo <bedro27@gmail.com>
2021-05-21 20:01:37 +0900

    feat: Logo 클릭 시 홈으로 이동하는 기능
    Co-authored-by: JUNMO HAN <wnsah052@naver.com>

--- src/@units/Header/Header.tsx
+++ src/@units/Header/Header.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import headerImg from 'assets/images/nanny.png';
+import { Link } from 'react-router-dom';
+import PATH from 'constants/PATH';
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -8,10 +10,12 @@ interface HeaderProps {
 const Header = ({ children }: HeaderProps) => {
   return (
     <header className="flex items-center justify-between p-4 bg-red-300">
-      <div className="flex items-center">
-        <img src={headerImg} alt="headerImg" className="w-16" />
-        <h1 className="-ml-2 text-2xl font-bold">Yum0 Map</h1>
-      </div>
+      <Link to={PATH.HOME}>
+        <div className="flex items-center">
+          <img src={headerImg} alt="headerImg" className="w-16" />
+          <h1 className="-ml-2 text-2xl font-bold">Yum0 Map</h1>
+        </div>
+      </Link>
       <nav>{children}</nav>
     </header>
   );
