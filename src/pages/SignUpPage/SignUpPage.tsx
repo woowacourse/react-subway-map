6e6cabbb218bfa4ca12b3ce8289313eb141e3ef8
Shim MunSeong <Puterism.K@gmail.com>
2021-05-21 17:13:55 +0900

    ui: 회원가입 페이지 UI 구현
    - 로그인 페이지에서 비밀번호 입력 폼에 `type="password"` 적용
    - 로그인 페이지에서 이메일 입력 폼에  `autoFocus` 적용
    
    Co-authored-by: dudtjr913 <dudtjr913@naver.com>

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Input, Button } from '../../components';
+import * as Styled from './SignUpPage.styles';
+import { ReactComponent as EmailIcon } from '../../assets/icons/envelope-solid.svg';
+import { ReactComponent as KeyIcon } from '../../assets/icons/key-solid.svg';
+import { ReactComponent as UserIcon } from '../../assets/icons/user-solid.svg';
 
 const SignUpPage = () => {
-  return <div>회원가입 페이지</div>;
+  return (
+    <Styled.SignUpPage>
+      <Styled.CardWrapper>
+        <Card>
+          <Styled.Form>
+            <Styled.HeaderText>회원가입</Styled.HeaderText>
+            <Styled.FormItem>
+              <Input icon={<EmailIcon />} placeholder="이메일을 입력해주세요" autoFocus />
+            </Styled.FormItem>
+            <Styled.FormItem>
+              <Input icon={<UserIcon />} placeholder="나이를 입력해주세요" />
+            </Styled.FormItem>
+            <Styled.FormItem>
+              <Input icon={<KeyIcon />} type="password" placeholder="비밀번호를 입력해주세요" />
+            </Styled.FormItem>
+            <Styled.FormItem>
+              <Input
+                icon={<KeyIcon />}
+                type="password"
+                placeholder="비밀번호를 한 번 더 입력해주세요"
+              />
+            </Styled.FormItem>
+            <Styled.FormItem>
+              <Styled.SignUpButton>
+                <Button fullWidth variant="primary">
+                  회원가입
+                </Button>
+              </Styled.SignUpButton>
+              <Link to="/">로그인으로 돌아가기</Link>
+            </Styled.FormItem>
+          </Styled.Form>
+        </Card>
+      </Styled.CardWrapper>
+    </Styled.SignUpPage>
+  );
 };
 
 export default SignUpPage;
