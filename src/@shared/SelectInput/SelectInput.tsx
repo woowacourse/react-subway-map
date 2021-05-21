7d5349577295e1f1963c2f3fc0656c5093fa016e
yujo <bedro27@gmail.com>
2021-05-21 17:12:27 +0900

    feat: SelectInput Component 구현
    Co-authored-by: JUNMO HAN <wnsah052@naver.com>

---
+++ src/@shared/SelectInput/SelectInput.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface SelectInputProps {
+  title?: string | null;
+  children?: React.ReactNode | null;
+}
+
+const SelectInput = ({ title, children }: SelectInputProps) => {
+  return (
+    <div className="flex items-center p-3 w-1/2 rounded ring-black ring-1">
+      {title && <div className="absolute left-8 top-2 px-1 text-black text-opacity-30 text-sm bg-white">{title}</div>}
+      <select className="ml-1 w-full focus:outline-none">{children && children}</select>
+    </div>
+  );
+};
+
+SelectInput.defaultProps = {
+  title: null,
+  children: null,
+};
+
+export default SelectInput;
