ec2514a7de76fe79d1a825694e5e830b306c2b2c
zereight <fullstackmachinelearning@gmail.com>
2021-05-21 17:44:09 +0900

    feat: Main 컴포넌트 구현

---
+++ src/components/atoms/Main/Main.styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+import { FlexCenterBox } from '../../../styles/css';
+
+const Container = styled.main`
+  width: 100%;
+  ${FlexCenterBox};
+  flex-direction: column;
+  background-color: #ffffff;
+  border-radius: 4px;
+  box-shadow: 0px 6px 21px 0px #ccc;
+`;
+
+export { Container };
