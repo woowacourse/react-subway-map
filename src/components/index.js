d104525c42f54e1898b6c64985a943098834aa5b
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-21 23:36:43 +0900

    feat: IconLogo 컴포넌트 생성

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { Button } from './atoms/Button';
+export { ButtonSquare } from './atoms/ButtonSquare';
+export { ButtonRound } from './atoms/ButtonRound';
+export { IconLogo } from './atoms/IconLogo';
+
+export { NavBar } from './molecules/NavBar';
