b8aad425c674144a44cd2d50b08298fd84094129
swon3210 <swon3210@gmail.com>
2021-05-22 20:26:03 +0900

    refactor: ThemeProvider, GlobalStyle 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 
-import { theme } from "./App.styles";
-import GlobalStyle from "./Global.styles";
 import LoginPage from "./pages/Login/LoginPage";
 import SignupPage from "./pages/Signup/SingupPage";
 import LineManagementPage from "./pages/LineManagement/LineManagementPage";
 import SectionManagementPage from "./pages/SectionManagement/SectionManagementPage";
 import StationManagementPage from "./pages/StationManagement/StationManagementPage";
 import SubwayMapPage from "./pages/SubwayMap/SubwayMapPage";
+import Header from "./components/Header/Header";
 
 enum PAGE_PATH {
   HOME = "/",
@@ -22,31 +20,29 @@ enum PAGE_PATH {
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Switch>
-          <Route path={PAGE_PATH.LOGIN}>
-            <LoginPage />
-          </Route>
-          <Route path={PAGE_PATH.SIGN_UP}>
-            <SignupPage />
-          </Route>
-          <Route path={[PAGE_PATH.HOME, PAGE_PATH.STATION_MANAGEMENT]}>
-            <StationManagementPage />
-          </Route>
-          <Route path={PAGE_PATH.LINE_MANAGEMENT}>
-            <LineManagementPage />
-          </Route>
-          <Route path={PAGE_PATH.SECTION_MANAGEMENT}>
-            <SectionManagementPage />
-          </Route>
-          <Route path={PAGE_PATH.SUBWAY_MANAGEMENT}>
-            <SubwayMapPage />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Header></Header>
+      <Switch>
+        <Route path={PAGE_PATH.LOGIN}>
+          <LoginPage />
+        </Route>
+        <Route path={PAGE_PATH.SIGN_UP}>
+          <SignupPage />
+        </Route>
+        <Route path={[PAGE_PATH.HOME, PAGE_PATH.STATION_MANAGEMENT]}>
+          <StationManagementPage />
+        </Route>
+        <Route path={PAGE_PATH.LINE_MANAGEMENT}>
+          <LineManagementPage />
+        </Route>
+        <Route path={PAGE_PATH.SECTION_MANAGEMENT}>
+          <SectionManagementPage />
+        </Route>
+        <Route path={PAGE_PATH.SUBWAY_MANAGEMENT}>
+          <SubwayMapPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,25 @@ import ReactDOM from "react-dom";
 import App from "./App";
 // import { store } from "./modules/store";
 import { Provider } from "react-redux";
+import { COLOR } from "./constants";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./Global.styles";
+
+export enum theme {
+  PRIMARY = COLOR.SKY_BLUE_500,
+  MAIN_TEXT_COLOR = COLOR.GRAY_900,
+  SUB_TEXT_COLOR = COLOR.GRAY_500,
+  BACKGROUND_COLOR = COLOR.GRAY_200,
+  DANGER_TEXT_COLOR = COLOR.RED_500,
+}
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-    <App />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
     {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById("root")
