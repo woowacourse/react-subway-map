8657f9ba03cf0144aeeb7377a146a030905ef4d4
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-22 22:20:38 +0900

    feat: Section 컴포넌트 생성
    Co-authored-by: 365kim <365kim@users.noreply.github.com>

---
+++ src/components/atoms/Section/style.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+import { COLOR } from '../../../constants/style';
+
+export const TopLine = styled.div`
+  width: 100%;
+  height: 1rem;
+  background-color: ${COLOR.THEME};
+  border-radius: 0.25rem 0.25rem 0 0;
+  border-color: ${COLOR.THEME};
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 3rem;
+  width: 100%;
+  max-width: 50rem;
+  min-height: 10rem;
+  background-color: #fff;
+  border: 0.0625rem solid ${COLOR.BORDER_DEFAULT};
+  border-radius: 0 0 0.25rem 0.25rem;
+`;
+
+export const Heading = styled.h2`
+  margin: 1rem 0;
+`;
+
+export const Content = styled.div``;
