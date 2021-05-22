f9a755834bdcb6bee606ac5abd4a7fbb83a4b5d4
Sunny.K <sunnyk5780@gmail.com>
2021-05-22 15:43:59 +0900

    feat: Icon 컴포넌트들 추가

---
+++ src/components/@common/Icon/Add.tsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import React, { FC } from 'react';
+import PALETTE from '../../../constants/palette';
+
+interface Props {
+  width?: string;
+  color?: string;
+}
+
+const Add: FC<Props> = ({ width, color = PALETTE.GRAY[400] }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width={width}
+      height={width}
+      fill={color}
+    >
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+    </svg>
+  );
+};
+
+Add.propTypes = {
+  width: PropTypes.string,
+  color: PropTypes.string,
+};
+
+export default Add;
---
+++ src/components/@common/Icon/Email.tsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import React, { FC } from 'react';
+import PALETTE from '../../../constants/palette';
+
+interface Props {
+  width?: string;
+  color?: string;
+}
+
+const Email: FC<Props> = ({ width, color = PALETTE.GRAY[400] }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width={width}
+      height={width}
+      fill={color}
+    >
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z" />
+    </svg>
+  );
+};
+
+Email.propTypes = {
+  width: PropTypes.string,
+  color: PropTypes.string,
+};
+
+export default Email;
