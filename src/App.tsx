675cafad7d70b4c740faad6c7da0ce36c5c8e87c
swon3210 <swon3210@gmail.com>
2021-05-22 22:17:07 +0900

    style: App 컴포넌트 return 문 제거

--- src/App.tsx
+++ src/App.tsx
@@ -11,41 +11,39 @@ import Button from "./components/Button/Button";
 import { Navigation } from "./App.styles";
 import { PAGE_PATH, navigationLinks } from "./constants/route";
 
-function App() {
-  const navigationButtons = navigationLinks.map((navigationLink) => (
-    <Link to={navigationLink.link}>
-      <Button buttonTheme="white" kind="rect">
-        {navigationLink.title}
-      </Button>
-    </Link>
-  ));
-
-  return (
-    <BrowserRouter>
-      <Header style={{ marginTop: "1.5625rem" }}>🚇 지하철 노선도</Header>
-      <Navigation>{navigationButtons}</Navigation>
-      <Switch>
-        <Route path={PAGE_PATH.LOGIN}>
-          <LoginPage />
-        </Route>
-        <Route path={PAGE_PATH.SIGN_UP}>
-          <SignupPage />
-        </Route>
-        <Route path={[PAGE_PATH.HOME, PAGE_PATH.STATION_MANAGEMENT]}>
-          <StationManagementPage />
-        </Route>
-        <Route path={PAGE_PATH.LINE_MANAGEMENT}>
-          <LineManagementPage />
-        </Route>
-        <Route path={PAGE_PATH.SECTION_MANAGEMENT}>
-          <SectionManagementPage />
-        </Route>
-        <Route path={PAGE_PATH.SUBWAY_MANAGEMENT}>
-          <SubwayMapPage />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Header style={{ marginTop: "1.5625rem", marginBottom: "1.5625rem" }}>🚇 지하철 노선도</Header>
+    <Navigation>
+      {navigationLinks.map((navigationLink) => (
+        <Link to={navigationLink.link}>
+          <Button buttonTheme="white" kind="rect">
+            {navigationLink.title}
+          </Button>
+        </Link>
+      ))}
+    </Navigation>
+    <Switch>
+      <Route path={PAGE_PATH.LOGIN}>
+        <LoginPage />
+      </Route>
+      <Route path={PAGE_PATH.SIGN_UP}>
+        <SignupPage />
+      </Route>
+      <Route path={[PAGE_PATH.HOME, PAGE_PATH.STATION_MANAGEMENT]}>
+        <StationManagementPage />
+      </Route>
+      <Route path={PAGE_PATH.LINE_MANAGEMENT}>
+        <LineManagementPage />
+      </Route>
+      <Route path={PAGE_PATH.SECTION_MANAGEMENT}>
+        <SectionManagementPage />
+      </Route>
+      <Route path={PAGE_PATH.SUBWAY_MANAGEMENT}>
+        <SubwayMapPage />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
