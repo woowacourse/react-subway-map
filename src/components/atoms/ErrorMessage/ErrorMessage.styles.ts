049d3d3cdd953bf30de6ded1c2f9f6061097a4c8
zereight <fullstackmachinelearning@gmail.com>
2021-05-22 17:32:14 +0900

    feat: ErrorMessage 컴포넌트 구현

---
+++ src/components/atoms/ErrorMessage/ErrorMessage.styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  width: 100%;
+  padding-left: 0.5rem;
+  text-align: left;
+`;
+
+const Message = styled.span`
+  color: #ef4444;
+`;
+
+export { Container, Message };
