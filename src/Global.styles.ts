458be1600ded1ea63f4b58ea46dab66e34d0f2cd
swon3210 <swon3210@gmail.com>
2021-05-22 22:18:23 +0900

    feat: heading 관련 css normalize 추가

--- src/Global.styles.ts
+++ src/Global.styles.ts
@@ -33,6 +33,16 @@ const customNormalize = css`
   a {
     text-decoration: none;
   }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+    padding: 0;
+  }
 `;
 
 const GlobalStyle = createGlobalStyle`
