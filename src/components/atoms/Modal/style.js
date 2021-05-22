4180a04dce642b453c7d2523652b09babc95836d
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-22 22:20:24 +0900

    feat: Modal 컴포넌트 생성
    Co-authored-by: 365kim <365kim@users.noreply.github.com>

---
+++ src/components/atoms/Modal/style.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const ModalContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 2;
+  background-color: rgba(0, 0, 0, 0.5);
+  transition: opacity 0.25s ease;
+`;
+
+export const ModalInner = styled.div`
+  position: relative;
+  overflow: auto;
+  min-width: 30rem;
+  border-radius: 0.3125rem;
+  transition: top 0.25s ease;
+
+  @media screen and (max-width: 768px) {
+    .modal-inner {
+      width: 90%;
+      height: 90%;
+      box-sizing: border-box;
+    }
+  }
+`;
