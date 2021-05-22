f62b238eff743d9de29f59eb3aa57da316486538
Kyle <mkitigy@gmail.com>
2021-05-22 13:21:27 +0900

    chore: commons -> @commons

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,8 @@ export { default as NavBar } from './NavBar';
 export { default as ManagementList } from './ManagementList';
 
 /* Common */
-export { default as PageTemplate } from './commons/PageTemplate';
-export { default as ModalTemplate } from './commons/ModalTemplate';
-export { default as Button } from './commons/Button';
-export { default as Input } from './commons/Input';
-export { default as Selector } from './commons/Selector';
+export { default as PageTemplate } from './@commons/PageTemplate';
+export { default as ModalTemplate } from './@commons/ModalTemplate';
+export { default as Button } from './@commons/Button';
+export { default as Input } from './@commons/Input';
+export { default as Selector } from './@commons/Selector';
