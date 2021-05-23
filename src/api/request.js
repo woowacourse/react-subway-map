22b6c701c44280b8b869404932a9e3e82c042dad
0imbean0 <tjsqls2003@gmail.com>
2021-05-23 18:15:20 +0900

    refactor: axios baseUrl 동적 변경 적용

--- src/api/request.js
+++ src/api/request.js
@@ -1,9 +1,29 @@
 import axios from 'axios';
+import { LS_KEY, SERVERS } from '../constants';
+import { getLocalStorage } from '../utils';
 
-const request = axios.create({
-  baseURL: 'https://jerry-subway.o-r.kr/',
-});
+const request = {
+  get: async (path) => {
+    const serverName = getLocalStorage(LS_KEY.SERVER);
 
-request.defaults.headers.post['Content-Type'] = 'application/json';
+    return await axios({
+      method: 'get',
+      url: path,
+      baseURL: SERVERS[serverName],
+    });
+  },
+
+  post: async (path, data) => {
+    const serverName = getLocalStorage(LS_KEY.SERVER);
+
+    return await axios({
+      method: 'post',
+      url: path,
+      baseURL: SERVERS[serverName],
+      headers: { 'content-type': 'application/json' },
+      data,
+    });
+  },
+};
 
 export default request;
