2dd1b0567b33718482ace3bcc761ab94506c9ab5
yungo1846 <yungo1468@naver.com>
2021-05-23 16:56:28 +0900

    refactor: sectionPage 디렉토리명을 PascalCase로 변경

---
+++ src/components/SectionPage/AddSectionForm.stories.tsx
@@ -0,0 +1,9 @@
+import AddSectionForm from './AddSectionForm';
+
+export default {
+  title: 'SectionPage/AddSectionForm',
+  component: AddSectionForm,
+};
+
+const Template = () => <AddSectionForm />;
+export const Default = Template.bind({});
---
+++ src/components/SectionPage/SectionListItem.tsx
@@ -0,0 +1,35 @@
+import * as S from './SectionListItem.styles';
+import trashCanSVG from '../../assets/svg/trash-can.svg';
+import editSVG from '../../assets/svg/edit.svg';
+
+export interface Props {
+  name: string;
+  distance?: number;
+  lineColor?: string;
+}
+
+const SectionListItem = ({ name, distance, lineColor }: Props) => {
+  return (
+    <S.SectionListItem>
+      <S.NameWrapper>
+        <S.Name lineColor={lineColor}>{name}</S.Name>
+        {distance && (
+          <S.Distance>
+            다음역까지
+            <br /> {distance}KM
+          </S.Distance>
+        )}
+      </S.NameWrapper>
+      <S.ButtonContainer>
+        <S.ButtonWrapper>
+          <S.Button src={editSVG} />
+        </S.ButtonWrapper>
+        <S.ButtonWrapper>
+          <S.Button src={trashCanSVG} />
+        </S.ButtonWrapper>
+      </S.ButtonContainer>
+    </S.SectionListItem>
+  );
+};
+
+export default SectionListItem;
