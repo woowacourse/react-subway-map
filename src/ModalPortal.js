d5ac07d4c091c05d9ce58a0b71bf14486413e523
shinsehantan <shinsehantan@users.noreply.github.com>
2021-05-23 23:06:58 +0900

    feat: ModalPortal 구현
    Co-authored-by: bigsaigon333 <likepeppermint@gmail.com>

---
+++ src/ModalPortal.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+
+const ModalPortal = ({ children }) => {
+  const $modal = document.getElementById("modal");
+
+  return ReactDOM.createPortal(children, $modal);
+};
+
+export default ModalPortal;
--- src/components/@shared/Modal/index.jsx
+++ src/components/@shared/Modal/index.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from "react";
 import PropTypes from "prop-types";
+import ModalPortal from "../../../ModalPortal";
 
 const Modal = ({ isOpen, children, close }) => {
   const handleClick = (event) => {
@@ -14,12 +15,14 @@ const Modal = ({ isOpen, children, close }) => {
 
   return (
     isOpen && (
-      <div
-        className="absolute left-0 top-0 flex items-center justify-center w-screen h-screen bg-black bg-opacity-40"
-        onClick={handleClick}
-      >
-        <div>{children}</div>
-      </div>
+      <ModalPortal>
+        <div
+          className="absolute left-0 top-0 flex items-center justify-center w-screen h-screen bg-black bg-opacity-40"
+          onClick={handleClick}
+        >
+          <div>{children}</div>
+        </div>
+      </ModalPortal>
     )
   );
 };
