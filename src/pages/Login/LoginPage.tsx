c16190df3100391043c2411b3632fe5e4faad8a8
swon3210 <swon3210@gmail.com>
2021-05-24 10:05:02 +0900

    feat: LoginPage & signupPage 컴포넌트 구현

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,7 +1,31 @@
-import { LoginPageBlock } from "./LoginPage.styles";
+import { Link } from "react-router-dom";
+
+import { Flex, FlexBetween, FlexCenter } from "../../components/@shared/FlexContainer/FlexContainer";
+import Block from "../../components/Block/Block";
+import Button from "../../components/Button/Button";
+import Input from "../../components/Input/Input";
+import { PAGE_PATH } from "../../constants/route";
 
 const LoginPage = () => {
-  return <LoginPageBlock>LoginPage</LoginPageBlock>;
+  return (
+    <FlexCenter>
+      <Block style={{ marginTop: "2.5rem", width: "540px", flexDirection: "column", alignItems: "flex-start" }}>
+        <FlexBetween style={{ width: "100%", marginBottom: "1rem" }}>
+          <h2 style={{ marginBottom: "1rem" }}>👋 로그인</h2>
+        </FlexBetween>
+        <Flex style={{ width: "100%", flexDirection: "column" }}>
+          <Input placeholder="이메일" style={{ marginBottom: "15px" }} />
+          <Input placeholder="비밀번호" style={{ marginBottom: "15px" }} />
+          <Button size="block" style={{ marginBottom: "15px" }}>
+            확인
+          </Button>
+          <p>
+            아직 회원이 아니신가요? <Link to={PAGE_PATH.SIGN_UP}>회원가입</Link>
+          </p>
+        </Flex>
+      </Block>
+    </FlexCenter>
+  );
 };
 
 export default LoginPage;
--- src/pages/Signup/SingupPage.tsx
+++ src/pages/Signup/SingupPage.tsx
@@ -1,7 +1,25 @@
-import { SignupPageBlock } from "./SignupPage.styles";
+import { Flex, FlexBetween, FlexCenter } from "../../components/@shared/FlexContainer/FlexContainer";
+import Block from "../../components/Block/Block";
+import Button from "../../components/Button/Button";
+import Input from "../../components/Input/Input";
 
 const SignupPage = () => {
-  return <SignupPageBlock>SignupPage</SignupPageBlock>;
+  return (
+    <FlexCenter>
+      <Block style={{ marginTop: "2.5rem", width: "540px", flexDirection: "column", alignItems: "flex-start" }}>
+        <FlexBetween style={{ width: "100%", marginBottom: "1rem" }}>
+          <h2 style={{ marginBottom: "1rem" }}>📝 회원가입</h2>
+        </FlexBetween>
+        <Flex style={{ width: "100%", flexDirection: "column" }}>
+          <Input placeholder="이메일" style={{ marginBottom: "15px" }} />
+          <Input placeholder="나이" style={{ marginBottom: "15px" }} />
+          <Input placeholder="비밀번호" style={{ marginBottom: "15px" }} />
+          <Input placeholder="비밀번호 확인" style={{ marginBottom: "25px" }} />
+          <Button size="block">확인</Button>
+        </Flex>
+      </Block>
+    </FlexCenter>
+  );
 };
 
 export default SignupPage;
