7f32fd9f62ad5b10fd7fd81844cdf4f4f4d7d5a2
Shim MunSeong <Puterism.K@gmail.com>
2021-05-24 14:53:34 +0900

    ui: 로그인, 회원가입에 서버를 선택할 수 있는 UI 구현
    Co-authored-by: dudtjr913 <dudtjr913@naver.com>

---
+++ src/constants/backend.ts
@@ -0,0 +1,20 @@
+const BACKEND = {
+  DANYEE: {
+    name: '다니',
+    baseUrl: 'https://da-nyee-subway-fare.kro.kr',
+  },
+  MARK: {
+    name: '마크',
+    baseUrl: 'https://binghe819-subway.p-e.kr',
+  },
+  CHARLIE: {
+    name: '찰리',
+    baseUrl: 'https://woowasubway.kro.kr',
+  },
+  YORN: {
+    name: '욘',
+    baseUrl: 'https://meto.r-e.kr',
+  },
+};
+
+export default BACKEND;
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Card, Input, Button } from '../../components';
+import { Card, Input, Button, Select } from '../../components';
 import * as Styled from './LoginPage.styles';
 import { ReactComponent as EmailIcon } from '../../assets/icons/envelope-solid.svg';
 import { ReactComponent as KeyIcon } from '../../assets/icons/key-solid.svg';
 import ROUTES from '../../constants/routes';
+import BACKEND from '../../constants/backend';
 
 const LoginPage = () => {
   return (
@@ -13,6 +14,13 @@ const LoginPage = () => {
         <Card>
           <Styled.Form>
             <Styled.HeaderText>로그인</Styled.HeaderText>
+            <Styled.FormItem>
+              <Select labelText="서버 선택">
+                {Object.entries(BACKEND).map(([crew, { name }]) => (
+                  <option value={crew}>{name}</option>
+                ))}
+              </Select>
+            </Styled.FormItem>
             <Styled.FormItem>
               <Input icon={<EmailIcon />} placeholder="이메일을 입력해주세요" autoFocus />
             </Styled.FormItem>
