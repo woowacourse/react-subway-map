72d7094a0976ae85087745c103567a41474cf4a6
shinsehantan <shinsehantan@users.noreply.github.com>
2021-05-24 16:50:57 +0900

    feat: 회원가입 페이지 이메일 입력창 유효성 검사
    Co-authored-by: bigsaigon333 <likepeppermint@gmail.com>

--- src/components/@shared/Input/index.jsx
+++ src/components/@shared/Input/index.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import cx from "classnames";
 
-const Input = ({ type, placeholder, onFocus }) => (
-  <input
-    type={type}
-    placeholder={placeholder}
-    className="px-4 py-2 w-full bg-transparent rounded outline-none ring-gray-300 focus:ring-yellow-300 ring-1 focus:ring-2"
-    onFocus={onFocus}
-  />
-);
+const getRingColor = (isFocused, isValid) => {
+  if (isFocused && !isValid) {
+    return "focus:ring-red-500 ring-red-500";
+  }
+
+  return "focus:ring-yellow-300 ring-gray-300";
+};
+
+const Input = ({ type, placeholder, onFocus, onChange, isValid, value }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = (event) => {
+    setIsFocused(true);
+    onFocus?.(event);
+  };
+
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      className={cx(
+        "px-4 py-2 w-full bg-transparent rounded outline-none ring-1 focus:ring-2",
+        getRingColor(isFocused, isValid)
+      )}
+      onFocus={handleFocus}
+      onChange={onChange}
+      value={value}
+    />
+  );
+};
 
 Input.propTypes = {
   type: PropTypes.oneOf(["number", "text", "email", "password"]).isRequired,
   placeholder: PropTypes.string,
   onFocus: PropTypes.func,
+  onChange: PropTypes.func,
+  isValid: PropTypes.bool,
+  value: PropTypes.string,
 };
 
 Input.defaultProps = {
   placeholder: null,
   onFocus: null,
+  onChange: null,
+  isValid: false,
+  value: "",
 };
 
 export default Input;
--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,45 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import Main from "../../components/@shared/Main";
 import Button from "../../components/@shared/Button";
 import Input from "../../components/@shared/Input";
 
-const Signup = () => (
-  <Main>
-    <section className="m-auto pb-8 w-120 border-t-8 border-yellow-300 rounded-sm shadow-md">
-      <h2 className="mb-4 mt-6 p-4 text-center text-gray-700 text-2xl font-medium">
-        회원가입
-      </h2>
-      <form className="flex flex-col px-8 space-y-8">
-        <label className="sr-only" htmlFor="email">
-          이메일 입력란
-        </label>
-        <Input id="email" type="email" placeholder="✉️ 이메일을 입력해주세요" />
-        <label className="sr-only" htmlFor="age">
-          나이 입력란
-        </label>
-        <Input id="age" type="number" placeholder="👤 나이를 입력해주세요" />
-        <label className="sr-only" htmlFor="password">
-          비밀번호 입력란
-        </label>
-        <Input
-          id="password"
-          type="password"
-          placeholder="🔒 비밀번호를 입력해주세요"
-        />
-        <label className="sr-only" htmlFor="password-confirm">
-          비밀번호 확인 입력란
-        </label>
-        <Input
-          id="password-confirm"
-          type="password"
-          placeholder="🔒 비밀번호를 한번 더 입력해주세요"
-        />
-        <Button type="submit" disabled={false}>
+const isValidEmail = (value) => {
+  const rEmail =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  return rEmail.test(value);
+};
+
+const Signup = () => {
+  const [Email, setEmail] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(false);
+
+  const handleEmailInput = (event) => {
+    const { value } = event.target;
+    const isValid = isValidEmail(value);
+
+    setIsEmailValid(isValid);
+    setEmail(value);
+  };
+
+  return (
+    <Main>
+      <section className="m-auto pb-8 w-120 border-t-8 border-yellow-300 rounded-sm shadow-md">
+        <h2 className="mb-4 mt-6 p-4 text-center text-gray-700 text-2xl font-medium">
           회원가입
-        </Button>
-      </form>
-    </section>
-  </Main>
-);
+        </h2>
+        <form className="flex flex-col px-8 space-y-8">
+          <label className="sr-only" htmlFor="email">
+            이메일 입력란
+          </label>
+          <Input
+            id="email"
+            type="email"
+            placeholder="✉️ 이메일을 입력해주세요"
+            value={Email}
+            isValid={isEmailValid}
+            onChange={handleEmailInput}
+          />
+          <label className="sr-only" htmlFor="age">
+            나이 입력란
+          </label>
+          <Input id="age" type="number" placeholder="👤 나이를 입력해주세요" />
+          <label className="sr-only" htmlFor="password">
+            비밀번호 입력란
+          </label>
+          <Input
+            id="password"
+            type="password"
+            placeholder="🔒 비밀번호를 입력해주세요"
+          />
+          <label className="sr-only" htmlFor="password-confirm">
+            비밀번호 확인 입력란
+          </label>
+          <Input
+            id="password-confirm"
+            type="password"
+            placeholder="🔒 비밀번호를 한번 더 입력해주세요"
+          />
+          <Button type="submit" disabled={false}>
+            회원가입
+          </Button>
+        </form>
+      </section>
+    </Main>
+  );
+};
 
 export default Signup;
