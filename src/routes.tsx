73e4bc91a8eeeec4f27f87306d1e7462e1e6a829
Shim MunSeong <Puterism.K@gmail.com>
2021-05-24 23:34:22 +0900

    feat: PrivateRoute 기능 구현
    - 역 관리, 노선 관리, 구간 관리 페이지에 접근할 때 로그인되어 있지 않으면 로그인 화면으로 리다이렉트
    
    Co-authored-by: dudtjr913 <dudtjr913@naver.com>

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,54 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, RouteProps, Switch } from 'react-router-dom';
+import LOCAL_STORAGE_KEYS from './constants/localStorageKeys';
 import ROUTES from './constants/routes';
+import useLocalStorage from './hooks/useLocalStorage';
 import LinePage from './pages/LinePage/LinePage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import SectionPage from './pages/SectionPage/SectionPage';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
 import StationPage from './pages/StationPage/StationPage';
 
+interface PrivateRouteIProps extends RouteProps {
+  children?: React.ReactNode;
+}
+
+const PrivateRoute = ({ children, ...props }: PrivateRouteIProps) => {
+  const [accessToken] = useLocalStorage(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
+  const [server] = useLocalStorage(LOCAL_STORAGE_KEYS.SERVER);
+
+  return (
+    <Route
+      {...props}
+      render={({ location }) =>
+        accessToken && server ? (
+          children
+        ) : (
+          <Redirect to={{ pathname: ROUTES.ROOT, state: { from: location } }} />
+        )
+      }
+    />
+  );
+};
+
 const Routes = (): React.ReactElement => {
   return (
     <Switch>
-      <Route exact path={ROUTES.ROOT} component={LoginPage} />
-      <Route exact path={ROUTES.SIGNUP} component={SignUpPage} />
-      <Route exact path={ROUTES.STATION} component={StationPage} />
-      <Route exact path={ROUTES.LINE} component={LinePage} />
-      <Route exact path={ROUTES.SECTION} component={SectionPage} />
+      <Route exact path={ROUTES.ROOT}>
+        <LoginPage />
+      </Route>
+      <Route exact path={ROUTES.SIGNUP}>
+        <SignUpPage />
+      </Route>
+      <PrivateRoute exact path={ROUTES.STATION}>
+        <StationPage />
+      </PrivateRoute>
+      <PrivateRoute exact path={ROUTES.LINE}>
+        <LinePage />
+      </PrivateRoute>
+      <PrivateRoute exact path={ROUTES.SECTION}>
+        <SectionPage />
+      </PrivateRoute>
     </Switch>
   );
 };
