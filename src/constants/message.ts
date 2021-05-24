1b61fbbafd1b9faf0f8492edca060a999f995a1b
Shim MunSeong <Puterism.K@gmail.com>
2021-05-24 22:55:11 +0900

    feat: 로그인 기능 구현
    Co-authored-by: dudtjr913 <dudtjr913@users.noreply.github.com>

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -3,7 +3,8 @@ const MESSAGE = {
     INVALID_EMAIL: '유효한 이메일을 입력해주세요',
     INVALID_PASSWORD: '8자 이상의 비밀번호를 입력해주세요',
     DIFFERENT_PASSWORD: '비밀번호가 서로 다르게 입력되었습니다',
-    DUPLICATED_EMAIL: '이미 등록되어 있는 이메일 주소입니다.',
+    DUPLICATED_EMAIL: '이미 등록되어 있는 이메일 주소입니다',
+    LOGIN_FAILURE: '로그인에 실패했습니다',
   },
 };
 
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,21 +1,66 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { Card, Input, Button, Select } from '../../components';
 import * as Styled from './LoginPage.styles';
 import { ReactComponent as EmailIcon } from '../../assets/icons/envelope-solid.svg';
 import { ReactComponent as KeyIcon } from '../../assets/icons/key-solid.svg';
+import useInput from '../../hooks/useInput';
+import REGEX from '../../constants/regex';
 import ROUTES from '../../constants/routes';
-import BACKEND from '../../constants/backend';
+import BACKEND, { CREWS } from '../../constants/backend';
+import MESSAGE from '../../constants/message';
+import API from '../../api';
+import useLocalStorage from '../../hooks/useLocalStorage';
 
 const LoginPage = () => {
+  const { value: selectedServer, onChange: onChangeSelectedServer } = useInput(CREWS.DANYEE);
+  const { value: email, onChange: onChangeEmail } = useInput('');
+  const { value: password, onChange: onChangePassword } = useInput('');
+  const [, setAccessToken] = useLocalStorage('accessToken');
+  const [, setServer] = useLocalStorage('server');
+
+  const history = useHistory();
+
+  const requestLogin = () => API[selectedServer].post('/login', { email, password });
+
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await requestLogin();
+      setAccessToken(response.data.accessToken);
+      setServer(selectedServer);
+
+      history.replace(ROUTES.STATION);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+
+      const message = error?.response?.data?.message;
+
+      if (message) {
+        // eslint-disable-next-line no-alert
+        alert(MESSAGE.ERROR.LOGIN_FAILURE);
+        return;
+      }
+
+      // eslint-disable-next-line no-alert
+      alert(message);
+    }
+  };
+
   return (
     <Styled.LoginPage>
       <Styled.CardWrapper>
         <Card>
-          <Styled.Form>
+          <Styled.Form onSubmit={handleLogin}>
             <Styled.HeaderText>로그인</Styled.HeaderText>
             <Styled.FormItem>
-              <Select labelText="서버 선택">
+              <Select
+                labelText="서버 선택"
+                value={selectedServer}
+                onChange={onChangeSelectedServer}
+              >
                 {Object.entries(BACKEND).map(([crew, { name }]) => (
                   <option key={crew} value={crew}>
                     {name}
@@ -24,10 +69,26 @@ const LoginPage = () => {
               </Select>
             </Styled.FormItem>
             <Styled.FormItem>
-              <Input icon={<EmailIcon />} placeholder="이메일을 입력해주세요" autoFocus />
+              <Input
+                value={email}
+                onChange={onChangeEmail}
+                icon={<EmailIcon />}
+                placeholder="이메일을 입력해주세요"
+                pattern={REGEX.isEmail.source}
+                required
+                autoFocus
+              />
             </Styled.FormItem>
             <Styled.FormItem>
-              <Input icon={<KeyIcon />} type="password" placeholder="비밀번호를 입력해주세요" />
+              <Input
+                value={password}
+                onChange={onChangePassword}
+                icon={<KeyIcon />}
+                type="password"
+                minLength={8}
+                placeholder="비밀번호를 입력해주세요"
+                required
+              />
             </Styled.FormItem>
             <Styled.FormItem>
               <Styled.LoginButton>
