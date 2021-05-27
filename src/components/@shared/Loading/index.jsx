26765ec4735e511850bd358d623eb30c36f5be97
shinsehantan <shinsehantan@users.noreply.github.com>
2021-05-28 00:37:20 +0900

    fix: Loading 컴포넌트 position absolute에서 fixed로 수정
    Co-authored-by: bigsaigon333 <likepeppermint@gmail.com>

--- src/components/@shared/Loading/index.jsx
+++ src/components/@shared/Loading/index.jsx
@@ -25,7 +25,7 @@ const Loading = ({ isLoading, bgOpacity }) =>
   isLoading && (
     <div
       className={cx(
-        "absolute left-0 top-0 flex items-center justify-center w-screen h-screen bg-black",
+        "fixed left-0 top-0 flex items-center justify-center w-screen h-screen bg-black",
         BG_OPACITY_STYLE[bgOpacity]
       )}
     >
