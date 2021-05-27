7936ccc841f38fb372559172e2d502c320bd6811
yungo1846 <yungo1468@naver.com>
2021-05-27 12:32:54 +0900

    fix: 디렉토리 이름 충돌 해결

--- src/components/sectionPage/SectionListItem.stories.tsx
+++
@@ -1,9 +0,0 @@
-import SectionListItem from './SectionListItem';
-
-export default {
-  title: 'SectionPage/SectionListItem',
-  component: SectionListItem,
-};
-
-const Template = () => <SectionListItem name='강남역' distance={2} handleDeleteSection={() => {}} />;
-export const Default = Template.bind({});
