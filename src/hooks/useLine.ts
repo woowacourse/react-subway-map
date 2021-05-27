ebe6d3f7124a534993d83dd66d58dc71c491a8d0
Shim MunSeong <Puterism.K@gmail.com>
2021-05-27 18:10:20 +0900

    refactor: 로그아웃 되었을 때 로그인 페이지로 이동하도록 변경 구현
    Co-authored-by: dudtjr913 <dudtjr913@naver.com>

--- src/hooks/useLine.ts
+++ src/hooks/useLine.ts
@@ -1,21 +1,23 @@
 import { useCallback, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from './useStore';
-import { Line, LineAttribute } from '../types';
+import { ApiStatus, Line, LineAttribute } from '../types';
 import { addLine, deleteLine, editLine, getLineList, resetError } from '../slices/lineSlice';
+import { logout } from '../slices/authSlice';
 import MESSAGE from '../constants/message';
 
 const useLine = () => {
   const line = useAppSelector((state) => state.line);
   const dispatch = useAppDispatch();
 
-  const { list, error } = line;
+  const { list, error, status } = line;
 
   const onGet = useCallback(() => dispatch(getLineList()), [dispatch]);
 
   const onAdd = ({ name, color, upStationId, downStationId, distance }: LineAttribute) =>
     dispatch(addLine({ name, color, upStationId, downStationId, distance }));
 
-  const onEdit = ({ id, name, color }: Line) => dispatch(editLine({ id, name, color }));
+  const onEdit = ({ id, name, color }: Pick<Line, 'id' | 'name' | 'color'>) =>
+    dispatch(editLine({ id, name, color }));
 
   const onDelete = (id: Line['id']) => dispatch(deleteLine(id));
 
@@ -27,6 +29,11 @@ const useLine = () => {
     if (error) {
       // eslint-disable-next-line no-alert
       alert(error.message || MESSAGE.ERROR.REQUEST_FAILURE);
+
+      if (error.status === 401) {
+        dispatch(logout());
+      }
+
       dispatch(resetError());
     }
   }, [error, dispatch]);
@@ -38,6 +45,8 @@ const useLine = () => {
     onDelete,
     list,
     error,
+    status,
+    isLoading: status === ApiStatus.IDLE || status === ApiStatus.PENDING,
   };
 };
 
