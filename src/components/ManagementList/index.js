0cd382a219404e18df47e52391d2eeaec13b2c8f
0imbean0 <tjsqls2003@gmail.com>
2021-05-27 21:06:20 +0900

    feat: 삭제 버튼에 ConfirmModal 연결

--- src/components/ManagementList/index.js
+++ src/components/ManagementList/index.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Container } from './style';
+import { ConfirmModal } from '..';
+import { useModal } from '../../hooks';
 
-const Item = ({ name, onDeleteItem }) => (
+const Item = ({ name, onClickDeleteButton }) => (
   <li>
     <span>{name}</span>
-    <button type="button" onClick={onDeleteItem}>
+    <button type="button" onClick={onClickDeleteButton}>
       🗑
     </button>
   </li>
 );
 
-const ManagementList = ({ items, onDeleteItem }) => (
-  <Container>
-    {items.map(({ id, name }) => (
-      <Item key={id} name={name} onDeleteItem={() => onDeleteItem({ id })} />
-    ))}
-  </Container>
-);
+const ManagementList = ({ items, onDeleteItem }) => {
+  const [deleteId, setDeleteId] = useState(null);
+  const { isModalOpen, openModal, closeModal } = useModal();
+
+  return (
+    <>
+      <Container>
+        {items.map(({ id, name }) => (
+          <Item
+            key={id}
+            name={name}
+            onClickDeleteButton={() => {
+              openModal();
+              setDeleteId(id);
+            }}
+          />
+        ))}
+      </Container>
+      {isModalOpen && (
+        <ConfirmModal
+          messages={['정말 삭제하시겠습니까?']}
+          closeModal={closeModal}
+          onConfirm={() => {
+            onDeleteItem({ id: deleteId });
+            setDeleteId(null);
+          }}
+        />
+      )}
+    </>
+  );
+};
 
 ManagementList.propTypes = {
   items: PropTypes.array.isRequired,
