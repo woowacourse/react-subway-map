cabd84d64c1ac3d2f282481a718d2993b33aae31
Sunny.K <sunnyk5780@gmail.com>
2021-05-27 16:08:25 +0900

    chore: API별 테마 색 변경
    Co-authored-by: Kwon Se-jin <0307kwon@users.noreply.github.com>

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -10,21 +10,21 @@ export const API_INFO: { [key: string]: APIInfo } = {
   FAFI: {
     name: '파피',
     endPoint: 'https://fafi-subway.o-r.kr',
-    themeColor: PALETTE.BLUE,
+    themeColor: PALETTE.GREEN,
   },
   JOEl: {
     name: '조엘',
     endPoint: 'https://joel-subway.kro.kr',
-    themeColor: PALETTE.ORANGE,
+    themeColor: PALETTE.YELLOW,
   },
   JOAN: {
     name: '조앤',
-    endPoint: 'https://anne-subway.r-e.kr ',
-    themeColor: PALETTE.GREEN,
+    endPoint: 'https://anne-subway.r-e.kr',
+    themeColor: PALETTE.RED,
   },
   JAYON: {
     name: '제이온',
     endPoint: 'https://jayon-subway.r-e.kr',
-    themeColor: PALETTE.PINK,
+    themeColor: PALETTE.CYAN,
   },
 };
