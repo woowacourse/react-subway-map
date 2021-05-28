6e03e2d2edf82bd2c3ecdbcfbf9830cc238ebb52
Kyle <mkitigy@gmail.com>
2021-05-28 09:42:40 +0900

    feat: Navbar 로그아웃 버튼에 ConfirmModal 연결

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
+import { Button, ConfirmModal } from '..';
 import { ROUTE } from '../../constants';
+import { useModal } from '../../hooks';
 import { Container, Logo, List } from './style';
 
 const publicRoutes = [ROUTE.SIGN_IN];
@@ -13,7 +15,7 @@ const privateRoutes = [
   ROUTE.SIGN_OUT,
 ];
 
-const Menu = () => {
+const Menu = ({ openModal }) => {
   const { token } = useSelector(({ user }) => user);
   const routes = token ? privateRoutes : publicRoutes;
 
@@ -21,31 +23,49 @@ const Menu = () => {
     <List>
       {routes.map(({ NAME, PATH }, index) => (
         <li key={index}>
-          <NavLink
-            to={PATH}
-            activeStyle={{
-              background: 'rgba(153, 153, 153, 0.3)',
-              borderRadius: '4px',
-            }}
-          >
-            {NAME}
-          </NavLink>
+          {NAME === ROUTE.SIGN_OUT.NAME ? (
+            <Button onClick={openModal}>{NAME}</Button>
+          ) : (
+            <NavLink
+              to={PATH}
+              activeStyle={{
+                background: 'rgba(153, 153, 153, 0.3)',
+                borderRadius: '4px',
+              }}
+            >
+              {NAME}
+            </NavLink>
+          )}
         </li>
       ))}
     </List>
   );
 };
 
-const NavBar = () => (
-  <Container>
-    <NavLink to={ROUTE.HOME.PATH}>
-      <Logo>
-        <img src="/images/logo_small.png" alt="logo" />
-        <span>RUNNING MAP</span>
-      </Logo>
-    </NavLink>
-    <Menu />
-  </Container>
-);
+const NavBar = () => {
+  const { isModalOpen, openModal, closeModal } = useModal();
+  const history = useHistory();
+
+  return (
+    <>
+      <Container>
+        <NavLink to={ROUTE.HOME.PATH}>
+          <Logo>
+            <img src="/images/logo_small.png" alt="logo" />
+            <span>RUNNING MAP</span>
+          </Logo>
+        </NavLink>
+        <Menu openModal={openModal} />
+      </Container>
+      {isModalOpen && (
+        <ConfirmModal
+          messages={['로그아웃 하시겠습니까?']}
+          closeModal={closeModal}
+          onConfirm={() => history.push(ROUTE.SIGN_OUT.PATH)}
+        />
+      )}
+    </>
+  );
+};
 
 export default NavBar;
