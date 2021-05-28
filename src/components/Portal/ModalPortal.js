01dac1dab72ba99d1212b28c1134cc1bcc903717
bigsaigon333 <likepeppermint@gmail.com>
2021-05-28 18:24:02 +0900

    refactor: 동적으로 HTMLElement를 마운트하도록 ModalPortal 을 수정한다
    Co-authored-by: shinsehantan <shinsehantan@users.noreply.github.com>

--- src/components/Portal/ModalPortal.js
+++ src/components/Portal/ModalPortal.js
@@ -1,7 +1,18 @@
 import ReactDOM from "react-dom";
+import { useEffect } from "react";
+
+const modalRoot = document.createElement("div");
+modalRoot.id = "modal-root";
+document.body.appendChild(modalRoot);
 
 const ModalPortal = ({ children }) => {
-  const $modal = document.getElementById("modal");
+  const $modal = document.createElement("div");
+
+  useEffect(() => {
+    modalRoot.appendChild($modal);
+
+    return () => modalRoot.removeChild($modal);
+  });
 
   return ReactDOM.createPortal(children, $modal);
 };
