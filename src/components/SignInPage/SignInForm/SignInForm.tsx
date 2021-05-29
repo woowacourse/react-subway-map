a5dcbcca6f4c3e9cd7720cff015002c5cebc7eee
Chayan <hchayan196@gmail.com>
2021-05-30 02:25:04 +0900

    fix: 불필요한 import 제거

--- src/components/SignInPage/SignInForm/SignInForm.tsx
+++ src/components/SignInPage/SignInForm/SignInForm.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import * as S from './SignInForm.styles';
 
 import mailSVG from '../../../assets/svg/mail.svg';
