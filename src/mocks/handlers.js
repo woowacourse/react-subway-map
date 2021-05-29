e17606bd0cb7c8b87bcc4944a832418cb3c32eaa
Shim MunSeong <Puterism.K@gmail.com>
2021-05-29 20:13:01 +0900

    test(refector): 역 관리 페이지 테스트 코드 항목 추가 및 중복되는 로직 함수 분리
    Co-authored-by: dudtjr913 <dudtjr913@naver.com>

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { rest } from 'msw';
 import BACKEND from '../constants/backend';
+import MESSAGE from '../constants/message';
 import { CREWS } from '../types';
 import { LINE_LIST, STATION_LIST } from './mockData';
 
@@ -9,6 +10,13 @@ export const handlers = [
     return res(ctx.status(200), ctx.json(STATION_LIST));
   }),
   rest.post(`${BACKEND[CREWS.DANYEE].baseUrl}/stations`, (req, res, ctx) => {
+    if (req.body.name.length < 2 || req.body.name.length > 20) {
+      return res(
+        ctx.status(400),
+        ctx.json({ status: 400, message: MESSAGE.ERROR.INVALID_STATION_NAME_LENGTH })
+      );
+    }
+
     const newStation = {
       id: 14,
       ...req.body,
@@ -17,6 +25,13 @@ export const handlers = [
     return res(ctx.status(201), ctx.json(newStation));
   }),
   rest.put(`${BACKEND[CREWS.DANYEE].baseUrl}/stations/:id`, (req, res, ctx) => {
+    if (req.body.name.length < 2 || req.body.name.length > 20) {
+      return res(
+        ctx.status(400),
+        ctx.json({ status: 400, message: MESSAGE.ERROR.INVALID_STATION_NAME_LENGTH })
+      );
+    }
+
     return res(
       ctx.status(200),
       ctx.json({
--- src/pages/StationPage/StationPage.test.js
+++ src/pages/StationPage/StationPage.test.js
@@ -11,75 +11,139 @@ import {
 } from '../../test-utils';
 import StationPage from './StationPage';
 import { STATION_LIST } from '../../mocks/mockData';
+import MESSAGE from '../../constants/message';
+
+const addStation = (name) => {
+  const stationNameInput = screen.getByRole('textbox', {
+    name: /지하철 역 이름을 입력해주세요/,
+  });
+
+  const addButton = screen.getByRole('button', {
+    name: '추가',
+  });
+
+  fireEvent.change(stationNameInput, {
+    target: {
+      value: name,
+    },
+  });
+  fireEvent.click(addButton);
+};
+
+const editStation = (targetStation, name) => {
+  const editModalOpenButton = getByLabelText(targetStation, '역 수정');
+
+  fireEvent.click(editModalOpenButton);
+
+  const editModal = screen.getByRole('dialog');
+  const editStationNameInput = getByPlaceholderText(editModal, '역 이름');
+  const editButton = getByRole(editModal, 'button', {
+    name: '수정',
+  });
+
+  fireEvent.change(editStationNameInput, {
+    target: { value: name },
+  });
+
+  fireEvent.click(editButton);
+};
 
 describe('지하철 역 관리', () => {
-  it('지하철 역 관리 페이지에서 역 목록을 조회한다', async () => {
+  beforeEach(() => {
     render(<StationPage />);
-
-    const stationList = await waitFor(() => screen.findAllByRole('listitem'));
-
-    expect(stationList).toHaveLength(STATION_LIST.length);
   });
 
-  it('지하철 역 추가 시 역 목록에서 확인할 수 있다', async () => {
-    render(<StationPage />);
+  describe('지하철 역 목록 조회', () => {
+    it('지하철 역 관리 페이지에서 역 목록을 조회한다.', async () => {
+      const stationList = await waitFor(() => screen.findAllByRole('listitem'));
 
-    const stationNameInput = screen.getByRole('textbox', {
-      name: /지하철 역 이름을 입력해주세요/,
+      expect(stationList).toHaveLength(STATION_LIST.length);
     });
-
-    const addButton = screen.getByRole('button', {
-      name: '추가',
-    });
-
-    fireEvent.change(stationNameInput, {
-      target: {
-        value: '디토',
-      },
-    });
-    fireEvent.click(addButton);
-
-    const ditto = await waitFor(() => screen.getByText('디토'));
-
-    expect(ditto).toBeInTheDocument();
   });
 
-  it('지하철 역 수정 시 정상적으로 역 이름이 변경된다.', async () => {
-    render(<StationPage />);
+  describe('지하철 역 추가', () => {
+    it('지하철 역 추가 시 역 목록에서 확인할 수 있다.', async () => {
+      const keyword = '디토';
+      addStation(keyword);
 
-    const stationList = await waitFor(() => screen.findAllByRole('listitem'));
-    const [targetStation] = stationList;
-
-    const editModalOpenButton = getByLabelText(targetStation, '역 수정');
-
-    fireEvent.click(editModalOpenButton);
-
-    const editModal = screen.getByRole('dialog');
-    const editStationNameInput = getByPlaceholderText(editModal, '역 이름');
-    const editButton = getByRole(editModal, 'button', {
-      name: '수정',
+      const ditto = await waitFor(() => screen.getByText(keyword));
+      expect(ditto).toBeInTheDocument();
     });
 
-    fireEvent.change(editStationNameInput, {
-      target: { value: '체프역' },
+    it('지하철 역 추가 시 1글자인 역 이름을 입력할 수 없으며, 에러 메시지를 확인할 수 있다.', async () => {
+      const keyword = '촆';
+      addStation(keyword);
+
+      const stationList = await waitFor(() => screen.findAllByRole('listitem'));
+      const alertMessage = await waitFor(() => screen.getByRole('alert'));
+
+      expect(alertMessage).toHaveTextContent(MESSAGE.ERROR.INVALID_STATION_NAME_LENGTH);
+      expect(stationList).not.toContain(new RegExp(keyword));
     });
 
-    fireEvent.click(editButton);
+    it('지하철 역 추가 시 21글자인 역 이름을 입력할 수 없으며, 에러 메시지를 확인할 수 있다.', async () => {
+      const keyword = '21글자이상인역이름을입력할수없습니다람쥐';
+      addStation(keyword);
 
-    const cheffe = await waitFor(() => screen.getByText('체프역'));
+      const stationList = await waitFor(() => screen.findAllByRole('listitem'));
+      const alertMessage = await waitFor(() => screen.getByRole('alert'));
 
-    expect(cheffe).toBeInTheDocument();
+      expect(alertMessage).toHaveTextContent(MESSAGE.ERROR.INVALID_STATION_NAME_LENGTH);
+      expect(stationList).not.toContain(new RegExp(keyword));
+    });
   });
 
-  it('지하철 역 삭제 시 유효한 삭제인지 검증한다.', async () => {
-    render(<StationPage />);
+  describe('지하철 역 수정', () => {
+    it('지하철 역 수정 시 정상적으로 역 이름이 변경된다.', async () => {
+      const keyword = '체프역';
 
-    const stationList = await waitFor(() => screen.findAllByRole('listitem'));
-    const [targetStation] = stationList;
-    const deleteButton = getByLabelText(targetStation, '역 삭제');
+      const stationList = await waitFor(() => screen.findAllByRole('listitem'));
+      const [targetStation] = stationList;
 
-    fireEvent.click(deleteButton);
+      editStation(targetStation, keyword);
 
-    await waitFor(() => expect(targetStation).not.toBeInTheDocument());
+      const editedStation = await waitFor(() => screen.getByText(keyword));
+      expect(editedStation).toBeInTheDocument();
+    });
+
+    it('지하철 역 수정 시 1글자인 역 이름을 입력할 수 없으며, 에러 메시지를 확인할 수 있다', async () => {
+      const keyword = '쳎';
+
+      const stationList = await waitFor(() => screen.findAllByRole('listitem'));
+      const [targetStation] = stationList;
+
+      editStation(targetStation, keyword);
+
+      const alertMessage = await waitFor(() => screen.getByRole('alert'));
+
+      expect(alertMessage).toHaveTextContent(MESSAGE.ERROR.INVALID_STATION_NAME_LENGTH);
+      expect(targetStation).not.toHaveTextContent(new RegExp(keyword));
+    });
+
+    it('지하철 역 수정 시 21글자인 역 이름을 입력할 수 없으며, 에러 메시지를 확인할 수 있다.', async () => {
+      const keyword = '21글자이상인역이름을입력할수없습니다람쥐';
+
+      const stationList = await waitFor(() => screen.findAllByRole('listitem'));
+      const [targetStation] = stationList;
+
+      editStation(targetStation, keyword);
+
+      const alertMessage = await waitFor(() => screen.getByRole('alert'));
+
+      expect(alertMessage).toHaveTextContent(MESSAGE.ERROR.INVALID_STATION_NAME_LENGTH);
+      expect(targetStation).not.toHaveTextContent(new RegExp(keyword));
+    });
+  });
+
+  describe('지하철 역 삭제', () => {
+    it('지하철 역 삭제 시 유효한 삭제인지 검증한다.', async () => {
+      const stationList = await waitFor(() => screen.findAllByRole('listitem'));
+      const [targetStation] = stationList;
+      const deleteButton = getByLabelText(targetStation, '역 삭제');
+
+      fireEvent.click(deleteButton);
+
+      await waitFor(() => expect(targetStation).not.toBeInTheDocument());
+    });
   });
 });
