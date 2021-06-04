91e74f81f5f9da1e3b7ea62807de64797d61b232
HyuuunjuKim <gus7wn@gmail.com>
2021-06-04 16:58:19 +0900

    refactor: Button 컴포넌트 Props 전부 내려 받도록 수정
    - extends ButtonHTMLAttributes

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -1,35 +1,27 @@
-import React from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 import Styled from './Button.styles';
 import { ButtonWidth, ButtonType } from 'types';
 
-interface Props {
-  type?: 'button' | 'submit' | 'reset';
-  disabled?: boolean;
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   styleType: ButtonType;
   widthType?: ButtonWidth;
   isSelected?: boolean;
   children: React.ReactNode;
-
-  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Button = ({
-  type,
-  disabled,
   children,
   styleType,
   widthType = ButtonWidth.AUTO,
   isSelected,
-  onClick,
+  ...props
 }: Props) => {
   return (
     <Styled.Container
-      type={type}
-      disabled={disabled}
       styleType={styleType}
       widthType={widthType}
       isSelected={isSelected}
-      onClick={onClick}
+      {...props}
     >
       {children}
     </Styled.Container>
