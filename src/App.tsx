cbc53390229b253e3dd89f7dd32ad68d52e93605
seojihwan <seojihwan213@naver.com>
2021-06-05 16:10:53 +0900

    refactor: route들 App에서 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import { useEffect } from "react";
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-  RouteProps,
-} from "react-router-dom";
+import { BrowserRouter, Switch, Link } from "react-router-dom";
 
-import LoginPage from "./pages/Login/LoginPage";
-import SignupPage from "./pages/Signup/SingupPage";
-import LineManagementPage from "./pages/LineManagement/LineManagementPage";
-import SectionManagementPage from "./pages/SectionManagement/SectionManagementPage";
-import StationManagementPage from "./pages/StationManagement/StationManagementPage";
-import SubwayMapPage from "./pages/SubwayMap/SubwayMapPage";
 import Header from "./components/Header/Header";
 import { PAGE_PATH, ROUTES } from "./constants/route";
 import useAuth from "./hooks/useAuth";
 import useStation from "./hooks/useStation";
 import useLine from "./hooks/useLine";
 import Navigation from "./components/Navigation/Navigation";
-import Logout from "./pages/Logout";
+import Routes from "./Routes";
 
 const App = () => {
   const { isAuthenticated, checkAccessToken, error: authError } = useAuth();
@@ -45,18 +32,6 @@ const App = () => {
     if (lineError) alert(lineError.message);
   }, [lineError]);
 
-  const PublicRoute = ({ children, ...props }: RouteProps) => (
-    <Route {...props}>
-      {isAuthenticated ? <Redirect to={PAGE_PATH.HOME} /> : children};
-    </Route>
-  );
-
-  const PrivateRoute = ({ children, ...props }: RouteProps) => (
-    <Route {...props}>
-      {isAuthenticated ? children : <Redirect to={PAGE_PATH.LOGIN} />};
-    </Route>
-  );
-
   return (
     <BrowserRouter>
       <Header style={{ marginTop: "1.5625rem", marginBottom: "1.5625rem" }}>
@@ -64,30 +39,7 @@ const App = () => {
       </Header>
       <Navigation routes={ROUTES} isAuthenticated={isAuthenticated} />
       <Switch>
-        <PublicRoute exact path={PAGE_PATH.LOGIN}>
-          <LoginPage />
-        </PublicRoute>
-        <PublicRoute exact path={PAGE_PATH.SIGN_UP}>
-          <SignupPage />
-        </PublicRoute>
-        <PrivateRoute exact path={PAGE_PATH.LOGOUT}>
-          <Logout />
-        </PrivateRoute>
-        <PrivateRoute
-          exact
-          path={[PAGE_PATH.HOME, PAGE_PATH.STATION_MANAGEMENT]}
-        >
-          <StationManagementPage />
-        </PrivateRoute>
-        <PrivateRoute exact path={PAGE_PATH.LINE_MANAGEMENT}>
-          <LineManagementPage />
-        </PrivateRoute>
-        <PrivateRoute exact path={PAGE_PATH.SECTION_MANAGEMENT}>
-          <SectionManagementPage />
-        </PrivateRoute>
-        <PrivateRoute exact path={PAGE_PATH.SUBWAY_MANAGEMENT}>
-          <SubwayMapPage />
-        </PrivateRoute>
+        <Routes isAuthenticated={isAuthenticated} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/Routes.tsx
@@ -0,0 +1,57 @@
+import { Route, RouteProps, Redirect } from "react-router";
+
+import LoginPage from "./pages/Login/LoginPage";
+import Logout from "./pages/Logout";
+import SignupPage from "./pages/Signup/SingupPage";
+import StationManagementPage from "./pages/StationManagement/StationManagementPage";
+import LineManagementPage from "./pages/LineManagement/LineManagementPage";
+import SectionManagementPage from "./pages/SectionManagement/SectionManagementPage";
+import SubwayMapPage from "./pages/SubwayMap/SubwayMapPage";
+
+import { PAGE_PATH } from "./constants/route";
+
+interface Props {
+  isAuthenticated: boolean;
+}
+
+const Routes = ({ isAuthenticated }: Props) => {
+  const PublicRoute = ({ children, ...props }: RouteProps) => (
+    <Route {...props}>
+      {isAuthenticated ? <Redirect to={PAGE_PATH.HOME} /> : children};
+    </Route>
+  );
+
+  const PrivateRoute = ({ children, ...props }: RouteProps) => (
+    <Route {...props}>
+      {isAuthenticated ? children : <Redirect to={PAGE_PATH.LOGIN} />};
+    </Route>
+  );
+
+  return (
+    <>
+      <PublicRoute exact path={PAGE_PATH.LOGIN}>
+        <LoginPage />
+      </PublicRoute>
+      <PrivateRoute exact path={PAGE_PATH.LOGOUT}>
+        <Logout />
+      </PrivateRoute>
+      <PublicRoute exact path={PAGE_PATH.SIGN_UP}>
+        <SignupPage />
+      </PublicRoute>
+      <PrivateRoute exact path={[PAGE_PATH.HOME, PAGE_PATH.STATION_MANAGEMENT]}>
+        <StationManagementPage />
+      </PrivateRoute>
+      <PrivateRoute exact path={PAGE_PATH.LINE_MANAGEMENT}>
+        <LineManagementPage />
+      </PrivateRoute>
+      <PrivateRoute exact path={PAGE_PATH.SECTION_MANAGEMENT}>
+        <SectionManagementPage />
+      </PrivateRoute>
+      <PrivateRoute exact path={PAGE_PATH.SUBWAY_MANAGEMENT}>
+        <SubwayMapPage />
+      </PrivateRoute>
+    </>
+  );
+};
+
+export default Routes;
