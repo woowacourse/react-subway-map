0157d33b0d4b1c52f406b102c7a542960c5292b6
yujo <bedro27@gmail.com>
2021-06-06 23:08:56 +0900

    feat: 전체보기 UI 구현

--- src/pages/SubwayMap.tsx
+++ src/pages/SubwayMap.tsx
@@ -1,6 +1,7 @@
 import Container from '@shared/Container/Container';
 import Title from '@shared/Title/Title';
-import ListItem from '@units/ListItem/ListItem';
+import MapItem from '@units/MapItem/MapItem';
+import { borderColor } from 'constants/color';
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getAllLinesAsync } from 'redux/lineSlice';
@@ -19,7 +20,14 @@ const SubwayMap = () => {
     <Container className="mb-16">
       <Title className="mb-8" text="🗺 전체 보기" />
       {lines?.map((line) => (
-        <h1>{line.name}</h1>
+        <Container key={line.id} className={`mt-6 w-full ${borderColor[line.color]}`}>
+          <div className={`flex justify-center items-center  rounded-2xl py-1 mb-6 ${line.color}`}>
+            <Title text={line.name} textSize="text-xl" className="text-center" />
+          </div>
+          <div className="flex">
+            <MapItem color={line.color} stations={line.stations} />
+          </div>
+        </Container>
       ))}
     </Container>
   );
