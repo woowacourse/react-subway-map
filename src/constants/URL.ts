569c7db486e5cfa0be892d2baa7a037608e4261d
yujo <bedro27@gmail.com>
2021-06-07 20:26:35 +0900

    chore: PK URL 변경

--- src/constants/URL.ts
+++ src/constants/URL.ts
@@ -1,7 +1,7 @@
 const URL = {
   WEDGE: 'https://wedge-subway.p-e.kr',
   KODA: 'https://techsubway.kro.kr',
-  PK: 'https://pkeugine-subway.kro.kr',
+  PK: 'https://pksubway.kro.kr',
   ROKI: 'https://rok-subway.kro.kr',
 };
 
