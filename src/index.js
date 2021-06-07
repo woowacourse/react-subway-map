b76c24c04187e5a5e6aa6420bc841c4bcf7e0cf4
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-06-07 12:44:50 +0900

    chore: js-cookie 설치 및 react-cookie 삭제

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import { CookiesProvider } from 'react-cookie';
 import { SnackbarProvider } from 'notistack';
 
 import App from './App';
@@ -13,11 +12,9 @@ import { GlobalStyle } from './style';
 ReactDOM.render(
   <SnackbarProvider maxSnack={3}>
     <Provider store={store}>
-      <CookiesProvider>
-        <Router>
-          <App />
-        </Router>
-      </CookiesProvider>
+      <Router>
+        <App />
+      </Router>
     </Provider>
     <GlobalStyle />
   </SnackbarProvider>,
