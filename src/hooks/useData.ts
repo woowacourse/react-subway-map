988c24a613c9c8d6055c663c4398a576ce111f49
jum0 <wnsah052@naver.com>
2021-06-11 15:52:47 +0900

    refactor: 불필요한 중복 type 선언 제거

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,14 +1,13 @@
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import { RootState } from 'redux/store';
-import { LineInterface, SelectedLineInterface, StationInterface } from 'types';
 
 const useData = () => {
   const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-  const accessToken: string | null = useAppSelector((state) => state.auth.accessToken);
-  const stations: StationInterface[] | null = useAppSelector((state) => state.station.stations);
-  const lines: LineInterface[] | null = useAppSelector((state) => state.line.lines);
-  const selectedLine: SelectedLineInterface | null = useAppSelector((state) => state.line.selectedLine);
+  const accessToken = useAppSelector((state) => state.auth.accessToken);
+  const stations = useAppSelector((state) => state.station.stations);
+  const lines = useAppSelector((state) => state.line.lines);
+  const selectedLine = useAppSelector((state) => state.line.selectedLine);
 
   return { accessToken, stations, lines, selectedLine };
 };
