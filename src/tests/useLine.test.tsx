3e5bf4e9e0460e93b1578f54a94058030ced0699
jho2301 <jho2301@gmail.com>
2021-06-16 20:53:48 +0900

    test: 노선 이름은 2자 이상 10자 이하의 한글이어야 한다.

--- src/tests/useLine.test.tsx
+++ src/tests/useLine.test.tsx
@@ -1,15 +1,20 @@
 import { renderHook } from '@testing-library/react-hooks';
 import { waitFor } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import { Wrapper, beforeEachFn } from './common';
 import { mockLines, mockToken } from '../mocks/mockData';
-import { act } from 'react-dom/test-utils';
 import useLine from '../service/hooks/useLine';
 import { LineForm } from '../types';
+import useLineAddForm from '../service/hooks/useLineAddForm';
+import { requestDeleteStation } from '../service/request/station';
 
 beforeEach(beforeEachFn);
 
 const renderUseLineHook = () =>
   renderHook(() => useLine(mockToken), { wrapper: Wrapper });
+
+const renderUseLineAddFormHook = () =>
+  renderHook(() => useLineAddForm(mockToken), { wrapper: Wrapper });
 
 describe('useLine', () => {
   test('사용자는 등록되어 있는 전체 지하철 노선 목록을 조회할 수 있다', async () => {
@@ -44,5 +49,25 @@ describe('useLine', () => {
     await waitFor(() =>
       expect(result.current.isDeleteLineSuccess).toBeTruthy()
     );
+  });
+});
+
+describe('useLineAddForm', () => {
+  test('노선 이름은 2자 이상 10자 이하의 한글이어야 한다.', async () => {
+    const { result } = renderUseLineAddFormHook();
+
+    const beneathMinLengthString = '일';
+    const overMaxLengthString = '일'.repeat(11);
+    const englishString = 'abc';
+
+    act(() => result.current.setName(beneathMinLengthString));
+
+    await waitFor(() => expect(result.current.isValidName).toBeFalsy());
+
+    act(() => result.current.setName(overMaxLengthString));
+    await waitFor(() => expect(result.current.isValidName).toBeFalsy());
+
+    act(() => result.current.setName(englishString));
+    await waitFor(() => expect(result.current.isValidName).toBeFalsy());
   });
 });
