dd6423e479a3296480a9ff9b2155774bbb8bb6a6
seojihwan <seojihwan213@naver.com>
2021-06-24 21:36:07 +0900

    feat: 전체 노선 조회기능 추가

--- src/pages/SubwayMap/SubwayMapPage.tsx
+++ src/pages/SubwayMap/SubwayMapPage.tsx
@@ -1,21 +1,32 @@
-import { Block } from "../../components";
+import { Block, Flex, Line } from "../../components";
 import { FlexCenter } from "../../components";
 
 import { SIZE } from "../../constants";
+import { useLine } from "../../hooks";
 
-const SubwayMapPage = () => (
-  <FlexCenter>
-    <Block
-      css={{
-        marginTop: "2.5rem",
-        width: SIZE.PAGE_CONTAINER_WIDTH,
-        flexDirection: "column",
-        alignItems: "flex-start",
-      }}
-    >
-      SubwayMapPage
-    </Block>
-  </FlexCenter>
-);
+const SubwayMapPage = () => {
+  const { lines } = useLine();
+
+  return (
+    <FlexCenter css={{ flexDirection: "column" }}>
+      <Block
+        css={{
+          marginTop: "2.5rem",
+          width: SIZE.PAGE_CONTAINER_WIDTH,
+          height: SIZE.PAGE_CONTAINER_HEIGHT,
+          overflow: "auto",
+          alignItems: "flex-start",
+          justifyContent: "start",
+        }}
+      >
+        <Flex css={{ flexDirection: "column" }}>
+          {lines.map((line) => (
+            <Line key={line.name} line={line} />
+          ))}
+        </Flex>
+      </Block>
+    </FlexCenter>
+  );
+};
 
 export default SubwayMapPage;
