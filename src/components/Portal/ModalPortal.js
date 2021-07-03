5b4d1290a354267e4e170d2bd4af98af76b0c5de
bigsaigon333 <likepeppermint@gmail.com>
2021-07-03 14:22:50 +0900

    fix: 모달 노드가 매 렌더링마다 추가되고 제거되던 오류를 수정한다

--- src/components/Portal/ModalPortal.js
+++ src/components/Portal/ModalPortal.js
@@ -1,20 +1,35 @@
 import { useEffect } from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 const modalRoot = document.createElement("div");
 modalRoot.id = "modal-root";
 document.body.appendChild(modalRoot);
 
-const ModalPortal = ({ children }) => {
-  const $modal = document.createElement("div");
+const ModalPortal = ({ children, id }) => {
+  let $modal = document.getElementById(id);
+
+  if (!$modal) {
+    $modal = document.createElement("div");
+    $modal.id = id;
+  }
 
   useEffect(() => {
     modalRoot.appendChild($modal);
 
     return () => modalRoot.removeChild($modal);
-  });
+  }, [$modal]);
 
   return ReactDOM.createPortal(children, $modal);
+};
+
+ModalPortal.propTypes = {
+  children: PropTypes.node.isRequired,
+  id: PropTypes.string,
+};
+
+ModalPortal.defaultProps = {
+  id: "modal",
 };
 
 export default ModalPortal;
