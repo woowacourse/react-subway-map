5971d1edbd93e05c0e1a3236f349520109404456
bigsaigon333 <likepeppermint@gmail.com>
2021-07-04 16:39:57 +0900

    refactor: 비정상적인 접근에 대한 알림에 Alert컴포넌트를 이용한다

--- src/components/@shared/PrivateRoute/index.jsx
+++ src/components/@shared/PrivateRoute/index.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Route, Redirect } from "react-router-dom";
+import { Route, useHistory } from "react-router-dom";
 import { useAuth } from "../../../pages/Login/hooks";
 import PATH from "../../../constants/path";
+import Alert from "../Alert";
 
 const PrivateRoute = ({ children, ...rest }) => {
+  const history = useHistory();
   const isAuthenticated = useAuth();
 
-  if (!isAuthenticated) {
-    alert("로그인이 필요합니다.");
-  }
+  const handleConfirm = () => {
+    history.push(PATH.LOGIN);
+  };
 
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <Route {...rest}>
-      {isAuthenticated ? children : <Redirect to={PATH.LOGIN} />}
+      {isAuthenticated ? (
+        children
+      ) : (
+        <Alert message="로그인이 필요합니다." onConfirm={handleConfirm} />
+      )}
     </Route>
   );
 };
