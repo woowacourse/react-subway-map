50f1ae0f07735eb430e1e10aee564107e6dd4ef3
bigsaigon333 <likepeppermint@gmail.com>
2021-07-04 16:13:48 +0900

    refactor: 회원가입 페이지에서 SignupMain을 분리한다

---
+++ src/components/SignupMain/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SignupForm from "../SignupForm/index";
+import Main from "../@shared/Main";
+
+const SignupMain = () => (
+  <Main className="relative">
+    <section className="m-auto pb-8 w-120 border-t-8 border-yellow-300 rounded-sm shadow-md">
+      <h2 className="mb-4 mt-6 p-4 text-center text-gray-700 text-2xl font-medium">
+        회원가입
+      </h2>
+      <SignupForm />
+    </section>
+  </Main>
+);
+
+export default SignupMain;
--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,44 +1,35 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import PATH from "../../constants/path";
 import STATUS from "../../constants/status";
-import { reset } from "./slice";
-import Main from "../../components/@shared/Main";
+import { reset, selectSignupMessage, selectSignupStatus } from "./slice";
 import Loading from "../../components/@shared/Loading";
-import SignupForm from "../../components/SignupForm";
+import Alert from "../../components/@shared/Alert";
+import SignupMain from "../../components/SignupMain";
 
 const Signup = () => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const status = useSelector(selectSignupStatus);
+  const message = useSelector(selectSignupMessage);
 
-  const { status, message } = useSelector((state) => state.signup);
+  const handleAlertConfirm = () => {
+    dispatch(reset());
 
-  useEffect(() => {
     if (status === STATUS.SUCCEED) {
-      alert(message);
-      dispatch(reset());
       history.push(PATH.LOGIN);
     }
-
-    if (status === STATUS.FAILED) {
-      alert(message);
-      dispatch(reset());
-    }
-
-    return () => dispatch(reset());
-  }, [status, message, dispatch, history]);
+  };
 
   return (
-    <Main className="relative">
+    <>
+      <Alert isOpen={Boolean(message)} onConfirm={handleAlertConfirm}>
+        <p className="text-lg">{message}</p>
+      </Alert>
       <Loading isLoading={status === STATUS.LOADING} />
-      <section className="m-auto pb-8 w-120 border-t-8 border-yellow-300 rounded-sm shadow-md">
-        <h2 className="mb-4 mt-6 p-4 text-center text-gray-700 text-2xl font-medium">
-          회원가입
-        </h2>
-        <SignupForm />
-      </section>
-    </Main>
+      <SignupMain />
+    </>
   );
 };
 
