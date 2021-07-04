81ede8eb192df763873436d46d3bd258e0905c11
bigsaigon333 <likepeppermint@gmail.com>
2021-07-04 16:20:51 +0900

    fix: 사용되지 않는 커스텀 훅을 제거한다

--- src/pages/Stations/hooks.js
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import useStatus from "../../hooks/useStatus";
-import { fetchStations, reset, selectStationsStatus } from "./slice";
-
-export const useStationsStatus = () => {
-  const status = useSelector(selectStationsStatus);
-
-  return useStatus({ status, fetch: fetchStations, reset });
-};
